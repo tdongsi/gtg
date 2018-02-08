ac15418b66267d04e87e8e82dfbb0a990f40587c
tdongsi <dongsi.tuecuong@gmail.com>
2018-02-07 20:46:53 -0800

    Simpler code.

--- anki/file.py
+++ anki/file.py
@@ -3,11 +3,9 @@ def mult_file(INFILE='numbers.txt', OUTFILE='numbers-mult.txt'):
     """Reads file numbers.txt and outputs where each number in numbers.txt is multiplied by its line number."""
 
     try:
-        count = 0
         with open(INFILE, 'r') as infile, open(OUTFILE, 'w') as outfile:
-            for line in infile:
+            for count, line in enumerate(infile, 1):
                 line.strip()
-                count += 1
                 if line:
                     out = count * int(line)
                     outfile.write("%d\n" % out)
