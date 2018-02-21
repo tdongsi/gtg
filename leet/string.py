9c0f90d4ace0ffc07a9fc7e226fc5e89c6301990
tdongsi <dongsi.tuecuong@gmail.com>
2018-02-21 00:31:52 -0800

    simple_string: Serious implementation.

--- leet/string.py
+++ leet/string.py
@@ -30,4 +30,37 @@ class Box():
     @staticmethod
     def simple_string_serious(mystring):
         """Evaluate simple expression string such as "5+6-11". Expected 11."""
+        def op(a, b, plus:bool)->int:
+            if plus:
+                return a + b
+            else:
+                return a - b
+
+        start = 0
+        end = -1
+        plus = True
+        sum = 0
+
+        for idx, char in enumerate(mystring):
+            if char == "+":
+                end = idx
+                if start < end:
+                    sum = op(sum, int(mystring[start:end]), plus)
+                start = idx + 1
+                plus = True
+            elif char == "-":
+                end = idx
+                if start < end:
+                    sum = op(sum, int(mystring[start:end]), plus)
+                start = idx+1
+                plus = False
+            else:
+                pass
+
+        # Handle the rest
+        if start < len(mystring):
+            sum = op(sum, int(mystring[start:]), plus)
+
+        return sum
+
         return 0
--- tests/leet/test_string.py
+++ tests/leet/test_string.py
@@ -11,3 +11,10 @@ class TestBox(TestCase):
         self.assertEqual(Box.simple_string(""), 0)
         self.assertEqual(Box.simple_string("5"), 5)
         self.assertEqual(Box.simple_string("-5+6"), 1)
+
+    def test_simple_string_serious(self):
+
+        self.assertEqual(Box.simple_string_serious("5+6-12"), -1)
+        self.assertEqual(Box.simple_string_serious(""), 0)
+        self.assertEqual(Box.simple_string_serious("5"), 5)
+        self.assertEqual(Box.simple_string_serious("-5+6"), 1)
