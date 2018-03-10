a120c51b3366e02bbeb5e32a346f62b1abfac149
tdongsi <dongsi.tuecuong@gmail.com>
2018-03-10 01:34:24 -0800

    Formal Subset Sum problem.

--- anki/dp.py
+++ anki/dp.py
@@ -7,7 +7,7 @@ def longest_common_subsequence(str1:str, str2:str) -> str:
     return ""
 
 
-def subset_sum(values:set, total:int) -> bool:
+def subset_sum(values: list, total: int) -> bool:
     """ Given an array of non-negative numbers and a total, is there a way to add up those numbers to the total.
 
     :param values: array of non-negative numbers
@@ -15,13 +15,21 @@ def subset_sum(values:set, total:int) -> bool:
     :return: True if possible
     """
 
-    check = [False] * (total+1)
-    check[0] = True
+    if not values:
+        return False
 
-    for i in range(1, total+1):
-        for val in values:
-            if i - val >= 0 and check[i-val]:
-                check[i] = True
-                break
+    n = len(values)
+    check = [[False] * (total+1) for _ in range(n+1)]
 
-    return check[total]
+    # if sum == 0, it is True no matter how many in the set
+    for i in range(n+1):
+        check[i][0] = True
+
+    for i in range(1, n+1):
+        for j in range(1, total+1):
+            if j < values[i-1]:
+                check[i][j] = check[i-1][j]
+            else:
+                check[i][j] = check[i-1][j] or check[i-1][j - values[i-1]]
+
+    return check[n][total]
--- tests/anki/test_dp.py
+++ tests/anki/test_dp.py
@@ -7,5 +7,10 @@ class TestDpExcercises(unittest.TestCase):
 
     def test_subset_sum(self):
 
-        self.assertEqual(True, dp.subset_sum({2, 3, 7, 8}, 11))
-        self.assertEqual(True, dp.subset_sum({3, 34, 4, 12, 5, 2}, 9))
+        self.assertEqual(True, dp.subset_sum([2, 3, 7, 8], 11))
+        self.assertEqual(True, dp.subset_sum([3, 34, 4, 12, 5, 2], 9))
+
+    def test_longest_common_subsequence_length(self):
+
+        self.assertEqual(3, dp.longest_common_subsequence_length("ABCDGH", "AEDFHR"))
+        self.assertEqual(4, dp.longest_common_subsequence_length("AGGTAB", "GXTXAYB"))
