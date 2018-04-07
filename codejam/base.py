3df6f61d5726efcf3a57663c0dd38cf81d87fe6a
tdongsi <dongsi.tuecuong@gmail.com>
2018-04-06 18:05:56 -0700

    Create abstract class BaseSolver

---
+++ codejam/base.py
@@ -0,0 +1,38 @@
+
+from abc import ABCMeta, abstractmethod
+import sys
+
+
+class BaseSolver(metaclass=ABCMeta):
+
+    def __init__(self, filename):
+        """ Initialize with the given input file.
+
+        :param filename: input file path
+        :return:
+        """
+        self._filename = filename
+        pass
+
+    def solve(self, output=sys.stdout):
+        """ Handle input and output before calling an internal method to solve the problem.
+        Follow Google Code Jam format.
+
+        :param output: specify output to file or screen.
+        :return:
+        """
+        try:
+            with open(self._filename, 'r') as f:
+                lines = f.readlines()
+
+                for case_num, line in enumerate(lines[1:], start=1):
+                    result = self._solve(line.strip())
+                    output.write("Case #%d: %s\n" % (case_num, result))
+
+        except IOError:
+            print("Error opening file")
+        pass
+
+    @abstractmethod
+    def _solve(self, input_str: str) -> str:
+        raise NotImplementedError('Abstract method')
--- codejam/submission.py
+++ codejam/submission.py
@@ -1,35 +1,8 @@
 
-import sys
+import codejam.base as bs
 
 
-class Solver():
-
-    def __init__(self, filename):
-        """ Initialize with the given input file.
-
-        :param filename: input file path
-        :return:
-        """
-        self._filename = filename
-        pass
-
-    def solve(self, output=sys.stdout):
-        """ Handle input and output before calling an internal method to solve the problem.
-
-        :param output: specify output to file or screen.
-        :return:
-        """
-        try:
-            with open(self._filename, 'r') as f:
-                lines = f.readlines()
-
-                for case_num, line in enumerate(lines[1:], start=1):
-                    result = self._solve(line.strip())
-                    output.write("Case #%d: %s\n" % (case_num, result))
-
-        except IOError:
-            print("Error opening file")
-        pass
+class Solver(bs.BaseSolver):
 
     def _solve(self, inputstr):
         return inputstr
