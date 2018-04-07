f106aeeec15b27f952994497d451ae9510c2cd42
tdongsi <dongsi.tuecuong@gmail.com>
2018-04-07 16:25:21 -0700

    Final submission for TroubleSort.

--- codejam/submission.py
+++ codejam/submission.py
@@ -1,9 +1,6 @@
 
 from abc import ABCMeta, abstractmethod
 import sys
-import itertools
-import math
-import decimal
 
 
 class BaseSolver(metaclass=ABCMeta):
@@ -29,7 +26,7 @@ class BaseSolver(metaclass=ABCMeta):
             with inf as f:
                 lines = f.readlines()
 
-                for case_num, line in enumerate(lines[1:], start=1):
+                for case_num, line in enumerate(lines[2::2], start=1):
                     result = self._solve(line.strip())
                     output.write("Case #%d: %s\n" % (case_num, result))
 
@@ -44,48 +41,36 @@ class BaseSolver(metaclass=ABCMeta):
 
 class Solver(BaseSolver):
     """ Google Code Jam 2018.
-    https://codejam.withgoogle.com/2018/challenges/00000000000000cb/dashboard/00000000000079cc
+    https://codejam.withgoogle.com/2018/challenges/00000000000000cb/dashboard/00000000000079cb
     """
 
     def _solve(self, inputstr):
-        area = float(inputstr)
-        centers = self._compute(area)
-        template = "\n{0} {1} {2}\n{3} {4} {5}\n{6} {7} {8}"
+        numbers = [int(e) for e in inputstr.split()]
+        sorted_nums = self._trouble_sort(numbers)
 
-        return template.format(*itertools.chain.from_iterable(centers))
+        idx = 'OK'
+        for i in range(len(numbers) - 1):
+            if sorted_nums[i] > sorted_nums[i + 1]:
+                return str(i)
 
-    def _compute(self, area: float):
-        """ Purely mathematical solution for Test set: 1.000000 <= A <= 1.414213.
-        For A <= 1.414213, it is satisfactory to just rotate around z axis.
-        One side of the projection will be always 1. The area is dependent on the other side length of projection
+        return idx
 
-        At the angle x = 0, the other side's length is 1.
-        For rotation of radian x, the other side's length is cos(x) + sin(x).
+    def _trouble_sort(self, mlist):
+        """ Trouble sorting is equivalent to sorting odd elements and even elements in place.
 
-        In that case, the output of three centers are as follows:
-        0.5*cos(x) 0.5*sin(x) 0
-        -0.5*sin(x) 0.5*cos(x) 0
-        0 0 0.5
-
-        The problem is reduced to given A = cos(x) + sin(x), find cos(x) and sin(x).
-        Given that 1 = cos(x) ^ 2 + sin(x) ^ 2, it can be solved using quadratic equation's formula OR
-        sin(2x) = A^2 - 1
-
-        :param area: input area
+        :param mlist:
         :return:
         """
-        k2 = decimal.Decimal(area*area)
-        one = decimal.Decimal(1.0)
-        alpha = math.asin(k2 - one)/2
+        temp = [0] * len(mlist)
+        temp[::2] = sorted(mlist[::2])
+        temp[1::2] = sorted(mlist[1::2])
 
-        cos = math.cos(alpha) * 0.5
-        sin = math.sin(alpha) * 0.5
+        return temp
 
-        return [(cos, sin, 0.0), (-sin, cos, 0.0), (0.0, 0.0, 0.5)]
 
 def main():
     """ Example run:
-    python3 codejam/submission.py < /Users/tdongsi/Hub/gtg/data/SaveTheUniverse.txt
+    python3 codejam/submission.py < /Users/tdongsi/Hub/gtg/data/TroubleSort.txt
     """
     solver = Solver()
     solver.solve()
