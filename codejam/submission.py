85c82f903b76ee344e65ff53aff8ee62684ad2ac
tdongsi <dongsi.tuecuong@gmail.com>
2018-04-06 23:33:18 -0700

    Naive submission.

--- codejam/submission.py
+++ codejam/submission.py
@@ -26,7 +26,7 @@ class BaseSolver(metaclass=ABCMeta):
             with inf as f:
                 lines = f.readlines()
 
-                for case_num, line in enumerate(lines[1:], start=1):
+                for case_num, line in enumerate(lines[2::2], start=1):
                     result = self._solve(line.strip())
                     output.write("Case #%d: %s\n" % (case_num, result))
 
@@ -40,58 +40,36 @@ class BaseSolver(metaclass=ABCMeta):
 
 
 class Solver(BaseSolver):
-    """
-    Google Code Jam 2018.
-    https://codejam.withgoogle.com/2018/challenges/00000000000000cb/dashboard
+    """ Google Code Jam 2018.
+    https://codejam.withgoogle.com/2018/challenges/00000000000000cb/dashboard/00000000000079cb
     """
 
     def _solve(self, inputstr):
-        limit, program = inputstr.split()
-        try:
-            count = self._compute(int(limit), program)
-            return str(count)
-        except ValueError:
-            return "IMPOSSIBLE"
+        numbers = [int(e) for e in inputstr.split()]
+        sorted_nums = self._trouble_sort(numbers)
 
-    def _compute_damage(self, program):
-        cur_hit = 1
-        total = 0
-        for c in program:
-            if c == 'C':
-                cur_hit *= 2
-            elif c == 'S':
-                total += cur_hit
-        return total
+        idx = 'OK'
+        for i in range(len(numbers) - 1):
+            if sorted_nums[i] > sorted_nums[i + 1]:
+                return str(i)
 
-    def _compute_charge(self, program):
-        cur_hit = 1
-        charge = [0] * len(program)
-        for idx, c in enumerate(program):
-            if c == 'C':
-                cur_hit *= 2
-            charge[idx] = cur_hit
-        return charge
+        return idx
 
-    def _compute(self, limit: int, program: str):
+    def _trouble_sort(self, mlist):
+        """Implementation of TroubleSort"""
+        done = False
 
-        count = 0
-        damage = self._compute_damage(program)
-        charge = self._compute_charge(program)
-        # print(charge)
-        while damage > limit:
-            idx = program.rfind('CS')
-            if idx == -1:
-                raise ValueError('Impossible')
-            else:
-                count += 1
-                program = program[:idx] + 'SC' + program[idx+2:]
+        if not mlist or len(mlist) <= 2:
+            return mlist
 
-                # damage recomputed more efficiently this way
-                # O(1) instead of O(n) if using function _compute_damage(program)
-                charge[idx] //= 2
-                damage -= charge[idx]
+        while not done:
+            done = True
+            for i in range(len(mlist) - 2):
+                if mlist[i] > mlist[i + 2]:
+                    mlist[i], mlist[i + 2] = mlist[i + 2], mlist[i]
+                    done = False
 
-        return count
+        return mlist
 
 
 def main():
