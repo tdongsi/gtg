d5de069bc94cc4230ae56673d70d8b81b24f21fa
tdongsi <dongsi.tuecuong@gmail.com>
2018-04-19 22:56:16 -0700

    Boyer-Moore algorithm.

--- chapter13/string.py
+++ chapter13/string.py
@@ -21,3 +21,38 @@ def find_brute(text:str, subs: str):
 
     return -1
 
+
+def find_boyer_moore(text:str, subs:str):
+    """ Pattern matching with Boyer-Moore algorithm.
+    Return the lowest index of text T at which substring P begins (or else -1).
+
+    :param text: text T
+    :param subs: substring P
+    :return: lowest index of T
+
+    """
+    if not text or not subs:
+        return -1
+
+    n, m = len(text), len(subs)
+
+    last = {}
+    for k in range(m):
+        last[subs[k]] = k
+
+    # align end of pattern at index m-1 of text
+    i = m-1  # index into 'text'
+    k = m-1  # index into 'subs'
+    while i < n:
+        if text[i] == subs[k]:  # a matching character
+            if k == 0:
+                return i  # pattern begins at index i of text
+            else:
+                i -= 1  # examine previous characters in both T & P
+                k -= 1
+        else:
+            j = last.get(text[i], -1)
+            i += m - min(k, j+1)
+            k = m -1  # restart at end of pattern
+
+    return -1
--- tests/chapter13/test_string.py
+++ tests/chapter13/test_string.py
@@ -10,3 +10,6 @@ class TestString(unittest.TestCase):
 
         self.assertEqual(gtg.find_brute('ANPANMAN', 'PAN'), 2)
         self.assertEqual(gtg.find_brute('abacaabaccabacabaabb', 'abacab'), 10)
+
+        self.assertEqual(gtg.find_boyer_moore('ANPANMAN', 'PAN'), 2)
+        self.assertEqual(gtg.find_boyer_moore('abacaabaccabacabaabb', 'abacab'), 10)
