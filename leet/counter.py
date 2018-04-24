203b03b318d76ece702a931359c5d13fff90d009
tdongsi <dongsi.tuecuong@gmail.com>
2018-04-23 23:02:41 -0700

    Check permutations.

--- leet/counter.py
+++ leet/counter.py
@@ -8,3 +8,14 @@ def find_uniques(mlist: list):
             out.append(k)
     return out
 
+
+def permutations(mlist: list):
+    if not mlist:
+        return
+
+    if len(mlist) == 1:
+        yield mlist
+    else:
+        for perm in permutations(mlist[1:]):
+            for i in range(len(mlist)):
+                yield perm[:i] + mlist[0:1] + perm[i:]
--- tests/leet/test_counter.py
+++ tests/leet/test_counter.py
@@ -8,3 +8,10 @@ class TestCounter(unittest.TestCase):
     def test_find_uniques(self):
         self.assertEqual(find_uniques([1,1,2,3,4,4]), [2,3])
         pass
+
+    def test_permutations(self):
+        for perm in permutations([1,2,3,4]):
+            print(perm)
+
+        all_perms = list(permutations([1,2,3]))
+        print(all_perms)
