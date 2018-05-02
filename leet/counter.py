baae5ee479b733112cb1bf385da22a5f1b042278
tdongsi <dongsi.tuecuong@gmail.com>
2018-05-02 04:12:52 -0700

    Counter exercises.

--- leet/counter.py
+++ leet/counter.py
@@ -2,10 +2,13 @@
 def find_uniques(mlist: list):
     from collections import Counter
     counter = Counter(mlist)
-    out = []
-    for k, v in counter.items():
-        if v == 1:
-            out.append(k)
-    return out
+    return [k for k, v in counter.items() if v == 1]
 
+
+def intersection(nums1, nums2):
+    from collections import Counter
+    counter1 = Counter(nums1)
+    counter2 = Counter(nums2)
+    counter12 = counter1 & counter2
+    return list(counter12.keys())
 
--- tests/leet/test_counter.py
+++ tests/leet/test_counter.py
@@ -7,4 +7,5 @@ class TestCounter(unittest.TestCase):
 
     def test_find_uniques(self):
         self.assertEqual(find_uniques([1,1,2,3,4,4]), [2,3])
+        self.assertEqual(intersection([1,2,2,1], [2,2]), [2])
         pass
