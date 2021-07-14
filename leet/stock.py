791c2c5b3d886599692882e3f32b7a2b6ee53965
tdongsi <dongsi.tuecuong@gmail.com>
2021-07-14 11:58:56 -0700

    Maximum subarray problem.

--- leet/stock.py
+++ leet/stock.py
@@ -26,6 +26,7 @@ def buy_stock(prices:list) -> Tuple[int, int, int]:
 
 def maximum_subarray_alt(numbers:list) -> list:
     """ Largest Sum Contiguous Subarray.
+    NOTE: This is a WRONG solution, based on the unit tests.
     https://en.wikipedia.org/wiki/Maximum_subarray_problem
     """
     total = 0
@@ -44,18 +45,22 @@ def maximum_subarray_alt(numbers:list) -> list:
 
 def maximum_subarray(nums: list) -> list:
     """Maximum subarray without using helper function buy_stock."""
-    total = nums[0]
-    gap, start, end = 0, 0, 0
-    min_val, min_idx = nums[0], 0
+    best_sum = 0  # NOTE: not infinity since you have a choice of empty sub-array.
+    best_start = best_end = 0
+    cur_sum, cur_start = 0, 0
 
-    for i in range(1, len(nums)):
-        total += nums[i]
+    for cur_end, e in enumerate(nums):
+        if cur_sum <= 0:
+            # Start a new sequence
+            cur_start = cur_end
+            cur_sum = e
+        else:
+            # Extend the current sequence
+            cur_sum += e
 
-        if total - min_val > gap:
-            gap = total - min_val
-            start, end = min_idx, i
+        if cur_sum > best_sum:
+            best_sum = cur_sum
+            best_start = cur_start
+            best_end = cur_end + 1
 
-        if total < min_val:
-            min_val, min_idx = total, i
-
-    return nums[start + 1: end + 1]
+    return nums[best_start:best_end]
--- tests/leet/test_stock.py
+++ tests/leet/test_stock.py
@@ -8,6 +8,8 @@ class TestStock(unittest.TestCase):
     def test_maximal_subarray(self):
         self.assertEqual([4, -1, -2, 1, 5], ls.maximum_subarray([-2, -3, 4, -1, -2, 1, 5, -3]))
         self.assertEqual([4, -1, 2, 1], ls.maximum_subarray([-2, 1, -3, 4, -1, 2, 1, -5, 4]))
+        self.assertEqual([4, -1, 2, 1, -5, 4, 5, 1], ls.maximum_subarray([-2, 1, -3, 4, -1, 2, 1, -5, 4, 5, 1, -3]))
+        self.assertEqual([4, 5, 1], ls.maximum_subarray([-2, 1, -3, 4, -1, 2, 1, -10, 4, 5, 1, -3]))
         self.assertEqual([5, -2, 9], ls.maximum_subarray([1, -3, 5, -2, 9, -8, -6, 4]))
 
         # Corner cases
@@ -15,9 +17,9 @@ class TestStock(unittest.TestCase):
         self.assertEqual([5], ls.maximum_subarray([5, -1]))
         self.assertEqual([5], ls.maximum_subarray([-1, 5]))
 
-        # self.assertEqual([], ls.maximum_subarray([-5]))
-        # self.assertEqual([], ls.maximum_subarray([-5, -1]))
-        # self.assertEqual([], ls.maximum_subarray([]))
+        self.assertEqual([], ls.maximum_subarray([-5]))
+        self.assertEqual([], ls.maximum_subarray([-5, -1]))
+        self.assertEqual([], ls.maximum_subarray([]))
         pass
 
     def test_maximal_subarray_alt(self):
